46cf383c182427354942827f41b5c02c43866088
kivanctezoren <58386759+kivanctezoren@users.noreply.github.com>
2022-06-12 02:44:00 +0300

    Finish implementation

--- resize-n-save.py
+++ resize-n-save.py
@@ -18,20 +18,18 @@ for dirpath, dirnames, filenames in os.walk(in_dir):
     for img_name in filenames:
         # Skips empty directories
         img_path = dirpath + "/" + img_name
-        resized_img = PILI.open(img_path).resize((new_size, new_size), PILI.ANTIALIAS)
+        resized_img = PILI.open(img_path).resize((new_size, new_size), PILI.ANTIALIAS).convert("RGB")
         
-        # Ensure png output
-        # TODO Does not consider formats other than jpg. Needs better filename detection
-        if img_name.endswith(".jpeg"):
-            resized_img_name = img_name[:-4] + "png"
-        elif img_name.endswith(".jpg"):
-            resized_img_name = img_name[:-3] + "png"
+        # Ensure jpg output
+        # TODO: Does not consider formats other than jpg. Needs better filename detection
+        if img_name.endswith(".png"):
+            resized_img_name = img_name[:-3] + "jpg"
         else:
             resized_img_name = img_name
         
-        save_dir = out_dir + dirpath[len(in_dir):] + resized_img_name
+        save_dir = out_dir + dirpath[len(in_dir):]
         os.makedirs(save_dir, exist_ok=True)
         
-        resized_img.save(save_dir)
-        
-        print("Saved image:", save_dir)
+        save_path = save_dir + "/" + resized_img_name
+        resized_img.save(save_path, subsampling=0, quality=100)  # Lose as little quality as possible
+        print("Saved image:", save_path)
